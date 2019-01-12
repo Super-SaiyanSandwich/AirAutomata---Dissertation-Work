9152fb01aa32ed6a41b84c09d690c49b4b99c6b9
Super-SaiyanSandwich <supersaiyansandwiches@gmail.com>
2019-01-12 11:40:00 +0000

    Update and rename test.py to dataReadin.py
    Neatened code and added commenting, docStrings and generally made more readable.
    
    
    nb/ not added doc-string for function "getLocations"

--- DataReadInPython/dataReadin.py
+++ DataReadInPython/dataReadin.py
@@ -5,7 +5,7 @@
 #
 import xml.etree.ElementTree as EleTree
 
-import numpy
+import numpy 
 
 import certifi
 import urllib3
@@ -21,6 +21,7 @@ import urllib3
 defraATOMUrlABD = "http://uk-air.defra.gov.uk/data/atom-dls/auto/2018/GB_FixedObservations_2018_ABD.atom.en.xml"
 defraATOMUrlABD2 = "http://uk-air.defra.gov.uk/data/atom-dls/observations/auto/GB_FixedObservations_2018_ABD.xml"
 
+## LIST OF NEEDED POLLUTANT URLS AND THE RELATED NAME
 pollutantCodes = {
     "http://dd.eionet.europa.eu/vocabulary/aq/pollutant/8":    "NO2",
     "http://dd.eionet.europa.eu/vocabulary/aq/pollutant/5":    "PM10",
@@ -29,11 +30,14 @@ pollutantCodes = {
     "http://dd.eionet.europa.eu/vocabulary/aq/pollutant/1":    "SO2"
 }
 
+## WITHIN THE DATASETS, IF A DATA SAMPLE HAS THIS CODE FOR ONE OF IT'S ATTRIBUTES THEN IT IS USELESS
 missingFOI = "http://environment.data.gov.uk/air-quality/so/GB_SamplingFeature_missingFOI"
 
 ##
 ##  ATOM feed codes
 ##
+"""  The following are the string codes used to identify elements with the DEFRA ATOM feed. 
+"""
 
 spatialDatasetCode = "{http://inspire.ec.europa.eu/schemas/inspire_dls/1.0}spatial_dataset_identifier_code"
 entry =  "{http://www.w3.org/2005/Atom}entry"
@@ -76,6 +80,29 @@ def getLocations(URL, httpCon, pollutantCodes):
 
 
 def getLocationData(httpCon, locationCode, pollutantCodes):
+    """
+    Downloads all of the data for a certain location.
+
+    By using the standardised Url naming system for the different locations, this function
+    downloads and parses the .XML file related to the year and location being saught.
+    The year is part of the location code in the form of "GB_FixedObservations_[year]_[location]"
+
+    Parameters
+    ----------
+    httpCon : urllib3.PoolManager
+        Used to access the URL and download the related file
+    locationCode : str
+        Location and year saught for data file
+    pollutantCodes : dict
+        Urls and related names of saught pollutants
+        
+    Returns
+    -------
+    dict
+        Dictionary of values for each pollutant
+
+    """
+    
     locationData = {}
 
     req = httpCon.request("GET", getLocationDataURL(locationCode))
@@ -90,29 +117,38 @@ def getLocationData(httpCon, locationCode, pollutantCodes):
     for obs in XMLdata.iter(observation):
         try:
             pollutant = obs[6].attrib[href]
+            
             if (pollutant in pollutantCodes) and (obs[7].attrib[href] != missingFOI):
+                
                 a = list(obs.iter(resultValues))[0]
                 data = a.text
                 data = data[20:].split("@@")
+                
                 for indxi, ite in enumerate(data):
                     data[indxi] = ite.split(",")   
-
+                
                 try:
                     locationData[pollutantCodes[pollutant]].append(data)   
                 except:
                     locationData[pollutantCodes[pollutant]] = data
+                    
         except KeyError:
             continue
 
     return locationData
         
             
-
+## Generates the main Url for the year of data being downloaded
 def getDataUrl(year):
     return "https://uk-air.defra.gov.uk/data/atom-dls/auto/"+ str(year) +"/atom.en.xml"
 
+## Generates the location Url for the year of data being downloaded
 def getLocationDataURL(locationID):
     return "https://uk-air.defra.gov.uk/data/atom-dls/observations/auto/" + locationID + ".xml"
+
+## Creates a Http connection object
+def getHttpCon():
+    return httpCon = urllib3.PoolManager(cert_reqs="CERT_REQUIRED",ca_certs=certifi.where())
 
 #
 #
@@ -120,67 +156,8 @@ def getLocationDataURL(locationID):
 #
 #
 
-print("::TEST::\n")
-
-httpCon = urllib3.PoolManager(cert_reqs="CERT_REQUIRED",ca_certs=certifi.where())
-
-
-"""
-req = httpCon.request("GET",defraATOMUrlABD2)
-reqList = str(req.data).split('\\n')
-reqList[0] = reqList[0][2:]
-reqList[-1] = reqList[-1][:-1]
-
-parser = EleTree.XMLParser(encoding="utf-8")
-ABDXMLdata = EleTree.fromstringlist(reqList, parser=parser)
-
-##with open(fileName,'w') as ABDfile:
-##    ABDfile.write(ABDXMLdata)
-
-
-
-for child in ABDXMLdata:
-    print(child.tag, child.text)
-    for subChild in child:
-        print("\t",subChild.tag, subChild.text)
-        for subsubChild in subChild:
-            print("\t\t",subsubChild.tag, subsubChild.attrib)
-            if (subsubChild.tag == "{http://www.opengis.net/om/2.0}result"):
-                for sssChild in subsubChild:
-                    print("\t\t\t",sssChild.tag,sssChild.text)
-
-print("\n\n::TEST 2::\n")
-
-
-featureOI = []
-t = []
-
-for ite in ABDXMLdata.iter('{http://www.opengis.net/om/2.0}featureOfInterest'):
-    feature = list(ite.attrib.values())[0]
-    featureOI.append(feature)
-    if (feature == 'http://environment.data.gov.uk/air-quality/so/GB_SamplingFeature_missingFOI'):
-        t.append(False)
-        continue
-    
-    
-t = numpy.array(featureOI)!='http://environment.data.gov.uk/air-quality/so/GB_SamplingFeature_missingFOI'
-
-c = 0
-values = []
-
-for indx, ite in enumerate(ABDXMLdata.iter('{http://www.opengis.net/swe/2.0}values')):
-    if t[indx]:
-        values.append(ite.text)
-
-
-for indx, data in enumerate(values):
-    data = data[20:].split("@@")
-    for indxi, ite in enumerate(data):
-        data[indxi] = ite.split(",")
-    values[indx] = data """
-    
-
-data = getLocations(getDataUrl(2019), httpCon, pollutantCodes)
-
-print("::TESTS END::")
+if __name__ == '__main__':
+    print("::TEST::\n")    
+    data = getLocations(getDataUrl(2019), getHttpCon(), pollutantCodes)
+    print("::TESTS END::")
 
