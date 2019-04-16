c967528fcb6505bcd1b149806d117849c5e71ec5
David Dixon [Sandwich] <dd331@exeter.ac.uk>
2019-04-16 12:12:37 +0100

    Fixed outlier issue

--- DataReadInPython/machineLearning.py
+++ DataReadInPython/machineLearning.py
@@ -24,6 +24,14 @@ deltaDay = 1 #Difference in days between day of data inputted and DAQI "forecast
 
 root = ""
 
+DAQIBands = [
+    [67,134,200,267,334,400,467,534,600], #NO2
+    [33,66,100,120,140,160,187,213,240], #Ozone
+    [16,33,50,58,66,75,83,91,100], #PM10
+    [11,23,35,41,47,53,58,64,70], #PM25
+    [88,177,266,354,443,532,710,887,1064] #SO2
+]
+
 # load dataset
 datasetPM25 = pandas.read_csv(root + 'DataSET-PM252017-2019.csv', header=0, index_col=0)
 datasetPM10 = pandas.read_csv(root + 'DataSET-PM102017-2019.csv', header=0, index_col=0)
@@ -37,11 +45,18 @@ def multitask_loss(y_true, y_pred):
 	# Multi-task loss
 	return K.mean(K.sum(- y_true * K.log(y_pred) - (1 - y_true) * K.log(1 - y_pred), axis=1))
 
-""" #reduce dataset
-datasetPM10 = datasetPM10.loc[:,datasetPM25.keys()]
-datasetOzone = datasetPM10.loc[:,datasetPM25.keys()]
-datasetNO2 = datasetPM10.loc[:,datasetPM25.keys()]
-datasetSO2 = datasetPM10.loc[:,datasetPM25.keys()] """
+def getDAQI(day, pollu):
+	d = [a for a in DAQIBands[pollu] if day < a]
+	return round(1.0 - (0.1 * len(d)),1)
+
+def removeOutliers(data): ##Removes any extreme Outliers in the data, as some of the datasets
+						  ##	are being suppressed by one or two extreme values
+	l = len(data)
+	datas = sorted(data)
+	q0 = datas[0]
+	q75 = datas[round(l * 0.75)]
+	flag = 2 * (q75 + 3 * (q75 - q0))
+	return array([(lambda x: x if x < flag else 0)(x) for x in data])
 
 sets = [datasetNO2, datasetOzone, datasetPM10, datasetPM25, datasetSO2]
 
@@ -51,8 +66,8 @@ keys = set.intersection(*[set(l) for l in sets])
 
 sets = [a.loc[:,keys] for a in sets]
 
-groups = [0, 1, 2, 3, 4 , 5, 6]
-groups = [i + 7 for i in groups]
+groups = [0, 1, 2, 3, 4 , 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+groups = [a + 15 for a in groups]
 
 """
 for dataset in sets:
@@ -78,15 +93,29 @@ data = datasetALL.values
 data = data.astype('float32')
 # normalize features
 
+dataY = array(data.reshape(round(data.shape[0]/timesteps), timesteps, features))
+
+#bigY = array([array([ [getDAQI(max(a[:,(i*locs) + j]),i) for j in range(locs)] for a in dataY]) for i in range(5)])
+scaler = [[]] * 5
+
 d = [[]]*5*locs
 for i in range(5):
-	scaler = MinMaxScaler(feature_range=(0, 1))
+	scaler[i] = MinMaxScaler(feature_range=(0, 1))
+
 	sec = data.T[i * locs:(i + 1) * locs,:]
+
 	secCon = array(list(reduce(chain, sec)))
+	secCon = removeOutliers(secCon)
 	secCon = secCon.reshape(-1, 1)
-	scaler = scaler.fit(secCon)
+
+	scaler[i] = scaler[i].fit(secCon)
+
+	pLoc = sec.shape[1]
+
+	sec = [secCon[j * pLoc:(j+1) * pLoc] for j in range(locs)]
+
 	for j in range(locs):
-		d[j + (i * locs)] = list(scaler.transform(array(sec[j]).reshape(-1,1)))
+		d[j + (i * locs)] = list(scaler[i].transform(array(sec[j]).reshape(-1,1)))
 
 d = array(d)
 e = d.reshape(5 * locs,data.shape[0]).T
@@ -103,7 +132,6 @@ pyplot.show()
 data = array(e.reshape(round(e.shape[0]/timesteps), timesteps, features))
 
 bigY = array([array([ [max(a[:,(i*locs) + j]) for j in range(locs)] for a in data]) for i in range(5)])
-
 
 bigY = bigY[:,deltaDay:]
 
@@ -142,28 +170,37 @@ outputLayer4 = createModel()
 outputLayer5 = createModel()
 
 model = Model(inputs=[i], outputs=[outputLayer1, outputLayer2, outputLayer3, outputLayer4, outputLayer5])
-model.compile(loss=multitask_loss, optimizer='adam', metrics=['accuracy'])
+model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
 
 #plot_model(model, to_file='model.png')
 
-epochs = 150
-batchSize = 32
+epochs = 100
+batchSize = 64
 
 history = model.fit(
 	x = trainX,
 	y = trainY,
 	batch_size = batchSize,
 	epochs = epochs,
-	verbose = 2,
+	verbose = 1,
 	shuffle = False,
 	validation_data=(testX, testY)
 )
 
-pyplot.plot(history.history['val_dense_6_acc'], label='Ozone')
+pyplot.figure()
+pyplot.subplot(2,1,1)
 pyplot.plot(history.history['val_dense_3_acc'], label='NO2')
+pyplot.plot(history.history['val_dense_6_acc'], label='Ozone')
 pyplot.plot(history.history['val_dense_9_acc'], label='PM10')
 pyplot.plot(history.history['val_dense_12_acc'], label='PM25')
 pyplot.plot(history.history['val_dense_15_acc'], label='SO2')
 pyplot.legend()
-pyplot.show()
 
+pyplot.subplot(2,1,2)
+pyplot.plot(history.history['dense_3_acc'], label='NO2')
+pyplot.plot(history.history['dense_6_acc'], label='Ozone')
+pyplot.plot(history.history['dense_9_acc'], label='PM10')
+pyplot.plot(history.history['dense_12_acc'], label='PM25')
+pyplot.plot(history.history['dense_15_acc'], label='SO2')
+pyplot.legend()
+pyplot.show()
